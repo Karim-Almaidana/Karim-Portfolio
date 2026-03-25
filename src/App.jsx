e4ab36d8325db5ebb56cdf65158e57e2bcac902c
Karim <kareem20224@gmail.com>
2026-03-25 22:43:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-[7xl]">
+      {/* navbar */}
+      <Navbar />
+      {/* hero */}
+      <Hero />
+      {/* about */}
+      <About />
+      {/* experience */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
+}
+
+export default App;
